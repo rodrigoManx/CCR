0f53277a2528cea9726c3f51e2b47b7ad66a24ab
rodrigoManx <rodrigo.pulcha@ucsp.edu.pe>
2017-03-20 01:14:05 -0500

    chat using sockets

---
+++ client.c
@@ -0,0 +1,100 @@
+ /* Client code in C */
+ 
+  #include <sys/types.h>
+  #include <sys/socket.h>
+  #include <netinet/in.h>
+  #include <arpa/inet.h>
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <string.h>
+  #include <unistd.h>
+  #include <pthread.h>
+
+  int SocketFD;
+  pthread_t recvThread;
+  int chatStatus = 0;
+
+  void chat();
+  void *recvMessage(void *ptr);
+
+  int main(int argc, char *argv[])
+  {
+    struct sockaddr_in stSockAddr;
+    int Res;
+    SocketFD = socket(AF_INET, SOCK_STREAM, 0); //IPPROTO_TCP
+    int n;
+    char buffer[256];
+    char message[256];
+    if (-1 == SocketFD)
+    {
+      perror("cannot create socket");
+      exit(EXIT_FAILURE);
+    }
+ 
+ 	
+ 	
+    memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
+ 
+    stSockAddr.sin_family = AF_INET;
+    stSockAddr.sin_port = htons(1100);
+    Res = inet_pton(AF_INET, "192.168.0.20", &stSockAddr.sin_addr);
+ 
+    if (0 > Res)
+    {
+      perror("error: first parameter is not a valid address family");
+      close(SocketFD);
+      exit(EXIT_FAILURE);
+    }
+    else if (0 == Res)
+    {
+      perror("char string (second parameter does not contain valid ipaddress");
+      close(SocketFD);
+      exit(EXIT_FAILURE);
+    }
+ 
+    if (-1 == connect(SocketFD, (const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in)))
+    {
+      perror("connect failed");
+      close(SocketFD);
+      exit(EXIT_FAILURE);
+    }
+
+    chat();
+    return 0;
+  }
+  void *recvMessage(void *ptr)
+  {
+    int n;
+    char buffer[250];
+
+    while(chatStatus == 1)
+    {
+      bzero(buffer, 250);
+      n = recv(SocketFD, buffer, 250, 0);
+      if (n < 0) perror("ERROR reading from socket");
+      printf("Server: %s\n", buffer);
+    } 
+  }
+
+  void chat()
+  {
+    pthread_create(&recvThread, NULL, recvMessage, NULL);
+    int n;
+    char message[250];
+    chatStatus = 1;
+
+    do
+    {
+      bzero(message, 250);
+      fgets(message, 250, stdin);
+      int messageSize = strlen(message);
+      if (messageSize > 1 && message[messageSize - 1] == '\n') 
+        message[messageSize - 1] = '\0';
+      n = send(SocketFD, message, strlen(message), 0);
+    }
+    while(strcmp(message, "END") != 0);
+    chatStatus = 0;
+    pthread_join(recvThread, NULL);
+    shutdown(SocketFD, SHUT_RDWR);
+    close(SocketFD);
+  }
---
+++ server.c
@@ -0,0 +1,108 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <pthread.h>
+
+int ConnectFD;
+pthread_t recvThread;
+int chatStatus = 0;
+
+void *recvMessage(void *ptr);
+void chatService();
+
+  int main(void)
+  {
+    struct sockaddr_in stSockAddr;
+    struct sockaddr_in cli_addr;
+    int client;
+    int SocketFD;
+    char buffer[256];
+    char message[256];
+    int Res;
+    int n;
+ 
+    if ((SocketFD = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("Socket");
+        exit(1);
+    }
+        
+    if (setsockopt(SocketFD,SOL_SOCKET,SO_REUSEADDR,"1",sizeof(int)) == -1) {
+        perror("Setsockopt");
+        exit(1);
+    }
+
+    memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
+		 
+    stSockAddr.sin_family = AF_INET;
+    stSockAddr.sin_port = htons(1100);
+    Res = inet_pton(AF_INET, "192.168.0.20", &stSockAddr.sin_addr);
+    //stSockAddr.sin_addr.s_addr = 192.168.0.20;
+
+    if (bind(SocketFD, (struct sockaddr *)&stSockAddr, sizeof(struct sockaddr))  == -1) {
+            perror("Unable to bind");
+            exit(1);
+        }
+
+        if (listen(SocketFD, 5) == -1) {
+            perror("Listen");
+            exit(1);
+        }
+ 
+    for(;;)
+    {
+
+    	client = sizeof(struct sockaddr_in);
+
+    	ConnectFD = accept(SocketFD, (struct sockaddr *)&cli_addr,&client);
+
+         
+        chatService();
+    }
+
+    close(SocketFD);
+    return 0;
+  }
+
+void *recvMessage(void *ptr)
+  {
+    chatStatus = 1;
+    int n;
+    char buffer[250];
+
+    do
+    {
+      bzero(buffer, 250);
+      n = recv(ConnectFD, buffer, 250, 0);
+      if (n < 0) perror("ERROR reading from socket");
+      printf("Client: %s\n", buffer);
+    }
+    while(strcmp(buffer, "END") != 0);
+
+    chatStatus = 0;
+  }
+
+  void chatService()
+  {
+    pthread_create(&recvThread, NULL, recvMessage, NULL);
+    int n;
+    char message[250];
+
+    do
+    {
+      bzero(message, 250);
+      fgets(message, 250, stdin);
+      int messageSize = strlen(message);
+      if (messageSize > 1 && message[messageSize - 1] == '\n') 
+        message[messageSize - 1] = '\0';
+      n = send(ConnectFD, message, strlen(message), 0);
+    }
+    while(chatStatus == 1);
+    pthread_join(recvThread, NULL);
+    close(ConnectFD);
+  }
